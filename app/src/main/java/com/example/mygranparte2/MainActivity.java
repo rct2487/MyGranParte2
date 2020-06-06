bf70d9f436014d3f685503ba26209617dd4b8c74
Rodrigo <rodri2487@gmail.com>
2020-06-05 21:08:28 -0500

    Añadiendo menu lateral

--- app/src/main/java/com/example/mygranparte2/MainActivity.java
+++ app/src/main/java/com/example/mygranparte2/MainActivity.java
@@ -1,18 +1,81 @@
 package com.example.mygranparte2;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawerlayout);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+// Navigation Drawer
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.drawer_open, R.string. drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(
+                R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_compartir:
+                Intent paramView;
+                paramView = new Intent("android.intent.action.SEND");
+                paramView.setType("text/plain");
+                paramView.putExtra("android.intent.extra.TEXT", "Descarga nuestra app de la PlayStore" +
+                        " \n" + "https://play.google.com/store/apps/details?id=app.product.demarktec.alo14_pasajero");
+                startActivity(Intent.createChooser(paramView, "Comparte Nuestro aplicativo"));
+                break;
+            case R.id.nav_salir:
+                finish();
+                break;
+            case R.id.nav_multimedia:
+                startActivity(new Intent(this, Multimedia.class));
+                break;
+            case R.id.nav_permisos:
+                startActivity(new Intent(this, Permisos.class));
+                break;
+            case R.id.nav_intenciones:
+                startActivity(new Intent(this, Intenciones.class));
+                break;
+            case R.id.nav_comunicacion:
+                startActivity(new Intent(this, Comunicacion1.class));
+                break;
+            default:
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(
+                R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
     @Override public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu1, menu);
@@ -43,6 +106,10 @@ public class MainActivity extends AppCompatActivity {
         }
         if (id == R.id.activity_multimedia) {
             startActivity(new Intent(this, Multimedia.class));
+            return true;
+        }
+        if (id == R.id.activity_permisos) {
+            startActivity(new Intent(this, Permisos.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
